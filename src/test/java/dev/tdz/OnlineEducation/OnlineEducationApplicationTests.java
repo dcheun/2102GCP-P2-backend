b7ee47f980e963ed1ed956946ce23fa0bfceb85a
Danny Cheun <dcheun@gmail.com>
2021-03-17 10:09:32 -0700

    Checking in skeleton Spring Boot project

---
+++ src/test/java/dev/tdz/OnlineEducation/OnlineEducationApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tdz.OnlineEducation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineEducationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
